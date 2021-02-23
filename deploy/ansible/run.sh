12b36d1da897dfb139f611630a28c55115018675
dirtrider <dirt-rider@yandex.ru>
2021-02-23 21:11:55 +0300

    Ansible deploy fixes; added README and LICENSE

--- deploy/ansible/run.sh
+++ deploy/ansible/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 ansible-galaxy role install -r requirements.yml
-ansible-playbook pb_deploy.yml --ask-vault-pass
+ansible-playbook pb_deploy.yml --ask-vault-pass --skip-tags postgres
+
+# Если требуется установить Postgres
+#ansible-playbook pb_deploy.yml --ask-vault-pass
