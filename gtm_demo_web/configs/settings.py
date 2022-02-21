d0c2ca43aead7609948716853cdf4411cdf6bcc9
Oleg Denisov <dirt-rider@yandex.ru>
2022-02-21 21:57:30 +0300

    Add CSRF_COOKIE_DOMAIN settings

--- gtm_demo_web/configs/settings.py
+++ gtm_demo_web/configs/settings.py
@@ -13,6 +13,8 @@ import json
 import os
 from pathlib import Path
 
+CSRF_COOKIE_DOMAIN = ".gtm-demo.ga"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
