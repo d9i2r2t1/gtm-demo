12f60c72164a075ffbe177a5fd1835bc54fc9bda
Oleg Denisov <dirt-rider@yandex.ru>
2022-02-21 22:19:22 +0300

    Add ensure_csrf_cookie

--- gtm_demo_web/configs/settings.py
+++ gtm_demo_web/configs/settings.py
@@ -13,8 +13,6 @@ import json
 import os
 from pathlib import Path
 
-CSRF_COOKIE_DOMAIN = ".gtm-demo.ga"
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- gtm_demo_web/gtm_demo/urls.py
+++ gtm_demo_web/gtm_demo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from gtm_demo.decorators import check_recaptcha
 from gtm_demo.views import MainView, DemoLandingView
@@ -6,6 +7,6 @@ from gtm_demo.views import MainView, DemoLandingView
 app_name = 'gtm_demo'
 
 urlpatterns = [
-    path('', check_recaptcha(MainView.as_view()), name='main'),
+    path('', ensure_csrf_cookie(check_recaptcha(MainView.as_view())), name='main'),
     path('<slug:hashcode>/', DemoLandingView.as_view(), name='demo_landing'),
 ]
